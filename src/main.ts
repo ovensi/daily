7de868bdff986d61886fc575ff3b5ea6891585f2
chenmeng <1487768100@qq.com>
2019-03-24 11:11:41 +0800

    feature: vue初始化

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,35 @@
 import Vue from 'vue';
+import FastClick from 'fastclick'; //解决iso点击300ms延迟
+import VueIconFont from 'vue-icon-font-pro'; //Iconfont图标库
+import vueEventCalendar from 'vue-event-calendar-pro'; //vue的简单日历事件
+import VueLazyComponent from "@xunlei/vue-lazy-component"; //vue组件懒加载
+import VueSkeletonLoading from 'vue-skeleton-loading';
+
+import 'normalize.css';
+import 'vue2-animate/dist/vue2-animate.min.css';//动画库
+import 'vue-event-calendar-pro/dist/style.css';
+
 import App from './App.vue';
-import router from './router';
-import store from './store';
 import './registerServiceWorker';
+import '@/assets/iconfont.js';
+import router from './router';
+
+if('addEventListener' in document){
+  document.addEventListener(
+    'DOMContentLoaded',function(){
+      (FastClick as any).attach(document.body)
+    },false
+  );
+}
+
+Vue.use(VueIconFont);
+Vue.use(vueEventCalendar),{locale:'zh',weekStartOn:1};
+Vue.use(VueLazyComponent);
+Vue.use(VueSkeletonLoading);
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
-  store,
-  render: (h) => h(App),
+  render: (h)=>h(App),
 }).$mount('#app');
---
+++ src/sw.js
@@ -0,0 +1,26 @@
+import monent from 'moment';
+
+workbox.loadModule('workbox-strateiges')
+
+const cacheVersion = '20181225V1';
+const StaticCacheName = 'static'+ cacheVersion;
+const staticAssetsCacheName = '/'+cacheVersion;
+const  vendorCacheName = 'vender' + cacheVersion;
+const maxEntries = 100 ;
+
+self._precacheManifest = [].contact(self._precacheManifest||[]);
+
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self._precacheManifest,[]
+  
+if(workbox){
+  console.log('Workbox is loaded');
+  workbox.precaching.precacheAndRoute(self._precacheManifest);
+}else{
+  console.log('Workbox didn`t load');
+}
+
+
+
+
+workbox.precaching.precacheAndRoute([])
